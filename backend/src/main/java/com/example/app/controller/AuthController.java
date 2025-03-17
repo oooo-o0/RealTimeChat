6944949792d222eb6400e2a4d72ca78619d7c55e
Sui <202876306+oooo-o0@users.noreply.github.com>
2025-03-17 10:07:17 +0900

    [ add ] 認証成功(ログイン・新規登録) / レスポンス統一モデル

--- backend/src/main/java/com/example/app/controller/AuthController.java
+++ backend/src/main/java/com/example/app/controller/AuthController.java
@@ -18,6 +18,7 @@ import com.example.app.config.CustomUserService;
 import com.example.app.config.TokenProvider;
 import com.example.app.exception.UserException;
 import com.example.app.model.User;
+import com.example.app.payload.AuthResponse;
 import com.example.app.repository.UserRepository;
 
 @RestController
