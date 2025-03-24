0df26f28127ff34f5cd06c51f38bbcffc8509d70
Sui <202876306+oooo-o0@users.noreply.github.com>
2025-03-24 15:28:06 +0900

    [ add ] ストーリ画像コンポーネントを作成

---
+++ frontend/src/components/Status/DummyStorage.jsx
@@ -0,0 +1,22 @@
+export const stories = [
+  {
+    // 1枚目のストーリー画像（イチゴの写真）
+    image: "https://cdn.pixabay.com/photo/2023/08/26/09/04/strawberry-8214486_640.jpg",
+  },
+  {
+    // 2枚目のストーリー画像（花の写真）
+    image: "https://cdn.pixabay.com/photo/2023/02/04/21/32/flowers-7768218_640.jpg",
+  },
+  {
+    // 3枚目のストーリー画像（女の子の写真）
+    image: "https://cdn.pixabay.com/photo/2023/09/04/10/16/girl-8232444_640.jpg",
+  },
+  {
+    // 4枚目のストーリー画像（カエルの写真）
+    image: "https://cdn.pixabay.com/photo/2022/10/07/01/15/frog-7503799_640.png",
+  },
+  {
+    // 5枚目のストーリー画像（別の花の写真）
+    image: "https://cdn.pixabay.com/photo/2023/09/11/13/01/flower-8246857_640.jpg",
+  },
+];
