9cb400dee3a164c28e41e764211b4b9912acfce1
Sui <202876306+oooo-o0@users.noreply.github.com>
2025-03-25 16:04:52 +0900

    [ update ] レンダリング設定の修正

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"; // React ライブラリをインポート
+import ReactDOM from "react-dom/client"; // React 18 では `react-dom/client` を使用して新しい API を活用
+import "./index.css"; // グローバルなスタイルを適用する CSS ファイルをインポート
+import App from "./App"; // アプリのルートコンポーネント `App` をインポート
+import { BrowserRouter } from "react-router-dom"; // クライアントサイドのルーティングを管理するための `BrowserRouter` をインポート
+import { Provider } from "react-redux"; // Redux の `Provider` をインポート（アプリ全体で Redux ストアを使えるようにする）
+import { store } from "./Redux/store"; // Redux のストア（`store`）をインポート
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// React 18 の新しい API を使用して `root` を作成
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// アプリのレンダリングを実行
 root.render(
   <React.StrictMode>
-    <App />
+    {/* ルーティングのために `BrowserRouter` でラップ */}
+    <BrowserRouter>
+      {/* Redux のストアをアプリ全体で利用できるように `Provider` でラップ */}
+      <Provider store={store}>
+        <App /> {/* ルートコンポーネント */}
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
