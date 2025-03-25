1d116c95e7114f8625badfe34dcc6332f49aaa91
Sui <202876306+oooo-o0@users.noreply.github.com>
2025-03-25 16:06:46 +0900

    [ update ] 微調整

--- frontend/src/components/Group/CreateGroup.jsx
+++ frontend/src/components/Group/CreateGroup.jsx
@@ -43,7 +43,7 @@ const CreateGroup = ({ setIsGroup }) => {
           <div className="flex items-center space-x-10 bg-[#069b60] text-white pt-16 px-10 pb-5">
             {/* 戻るボタン（`BsArrowLeft` アイコン） */}
             <BsArrowLeft className="cursor-pointer text-2xl font-bold" />
-            <p className="text-xl font-semibold">Add Participants</p>
+            <p className="text-xl font-semibold">新しくチャットを作成</p>
           </div>
 
           {/* ユーザー検索エリア */}
@@ -58,7 +58,7 @@ const CreateGroup = ({ setIsGroup }) => {
             <input
               type="text"
               className="outline-none border-b border-[#8888] p-2 w-[93%]"
-              placeholder="Search user"
+              placeholder="ユーザーを検索"
               value={query} // 入力値を`query`の状態にバインド
               onChange={(e) => {
                 handleSearch(e.target.value); // ユーザー検索を実行
